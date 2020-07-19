4f3c618d995b8464c0b98c2899ca15e3841c7c03
Matan Shaviro <matanshaviro@matans-mbp-2.mynet>
2020-07-19 15:58:52 +0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import requests from './requests';
+import Row from './components/Row';
+import Banner from './components/Banner';
+import Nav from './components/Nav';
+
 function App() {
+  const {
+    fetchNetflixOriginals,
+    fetchTrending,
+    fetchTopRated,
+    fetchActionMovies,
+    fetchComedyMovies,
+    fetchHorrorMovies,
+    fetchRomanceMovies,
+    fetchDocumentaries
+  } = requests;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Nav />
+      <Banner />
+      <Row
+        title='NETFLIX ORIGINALS'
+        fetchUrl={fetchNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title='Trending Now' fetchUrl={fetchTrending} />
+      <Row title='Top Rated' fetchUrl={fetchTopRated} />
+      <Row title='Action Movies' fetchUrl={fetchActionMovies} />
+      <Row title='Comedy Movies' fetchUrl={fetchComedyMovies} />
+      <Row title='Horror Movies' fetchUrl={fetchHorrorMovies} />
+      <Row title='Romance Movies' fetchUrl={fetchRomanceMovies} />
+      <Row title='Documentaries' fetchUrl={fetchDocumentaries} />
     </div>
   );
 }
